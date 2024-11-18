ad21a981025f112e72f7230176199733bf892418
yahav20 <yahavezer256@gmail.com>
2024-11-19 00:01:06 +0200

    Making combine docker

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -5,7 +5,7 @@ TEST(AdditionTest , PositiveNumbers){
     EXPECT_EQ(add(1,2),3);
 }
 
-int main(int argc , char **argv){
-    ::testing::InitGoogleTest(&args,argv);
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
